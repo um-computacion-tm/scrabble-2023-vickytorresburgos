2a633dc4829f734048a7da51b15f33fbb5e580d5
vicky <vicky@pop-os.localdomain>
2023-08-22 10:32:35 -0300

    agregue tests

--- tests/test_scrabble.py
+++ tests/test_scrabble.py
@@ -34,6 +34,143 @@ class TestTiles(unittest.TestCase):
         self.assertEqual(tile.value,2)
         self.assertEqual(tile.cant,5)
 
+    def test_tileE(self):
+        tile = Tile('E',1,12)
+        self.assertEqual(tile.letter, 'E')
+        self.assertEqual(tile.value,1)
+        self.assertEqual(tile.cant,12)
+
+    def test_tileF(self):
+        tile = Tile('F',4,1)
+        self.assertEqual(tile.letter, 'F')
+        self.assertEqual(tile.value,4)
+        self.assertEqual(tile.cant,1)
+
+    def test_tileG(self):
+        tile = Tile('G',2,2)
+        self.assertEqual(tile.letter, 'G')
+        self.assertEqual(tile.value,2)
+        self.assertEqual(tile.cant,2)
+
+    def test_tileH(self):
+        tile = Tile('H',4,2)
+        self.assertEqual(tile.letter, 'H')
+        self.assertEqual(tile.value,4)
+        self.assertEqual(tile.cant,2)
+
+    def test_tileI(self):
+        tile = Tile('I',1,6)
+        self.assertEqual(tile.letter, 'I')
+        self.assertEqual(tile.value,1)
+        self.assertEqual(tile.cant,6)
+    
+    def test_tileJ(self):
+        tile = Tile('J',8,1)
+        self.assertEqual(tile.letter, 'J')
+        self.assertEqual(tile.value,8)
+        self.assertEqual(tile.cant,1)
+
+    def test_tileL(self):
+            tile = Tile('L',1,4)
+            self.assertEqual(tile.letter, 'L')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,4)
+
+    def test_tileLL(self):
+            tile = Tile('LL',8,1)
+            self.assertEqual(tile.letter, 'LL')
+            self.assertEqual(tile.value,8)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileM(self):
+            tile = Tile('M',3,2)
+            self.assertEqual(tile.letter, 'M')
+            self.assertEqual(tile.value,3)
+            self.assertEqual(tile.cant,2)
+
+    def test_tileN(self):
+            tile = Tile('N',1,5)
+            self.assertEqual(tile.letter, 'N')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,5)
+
+    def test_tileÑ(self):
+            tile = Tile('Ñ',1,5)
+            self.assertEqual(tile.letter, 'Ñ')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,5)
+
+    def test_tileO(self):
+            tile = Tile('O',1,9)
+            self.assertEqual(tile.letter, 'O')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,9)
+
+    def test_tileP(self):
+            tile = Tile('P',3,2)
+            self.assertEqual(tile.letter, 'P')
+            self.assertEqual(tile.value,3)
+            self.assertEqual(tile.cant,2)
+
+    def test_tileQ(self):
+            tile = Tile('Q',5,1)
+            self.assertEqual(tile.letter, 'Q')
+            self.assertEqual(tile.value,5)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileR(self):
+            tile = Tile('R',1,5)
+            self.assertEqual(tile.letter, 'R')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,5)
+
+    def test_tileRR(self):
+            tile = Tile('RR',8,1)
+            self.assertEqual(tile.letter, 'RR')
+            self.assertEqual(tile.value,8)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileS(self):
+            tile = Tile('S',1,6)
+            self.assertEqual(tile.letter, 'S')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,6)
+
+    def test_tileT(self):
+            tile = Tile('T',1,4)
+            self.assertEqual(tile.letter, 'T')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,4)
+
+    def test_tileU(self):
+            tile = Tile('U',1,5)
+            self.assertEqual(tile.letter, 'U')
+            self.assertEqual(tile.value,1)
+            self.assertEqual(tile.cant,5)
+
+    def test_tileV(self):
+            tile = Tile('V',4,1)
+            self.assertEqual(tile.letter, 'V')
+            self.assertEqual(tile.value,4)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileX(self):
+            tile = Tile('X',8,1)
+            self.assertEqual(tile.letter, 'X')
+            self.assertEqual(tile.value,8)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileY(self):
+            tile = Tile('Y',4,1)
+            self.assertEqual(tile.letter, 'Y')
+            self.assertEqual(tile.value,4)
+            self.assertEqual(tile.cant,1)
+
+    def test_tileZ(self):
+            tile = Tile('Z',10,1)
+            self.assertEqual(tile.letter, 'Z')
+            self.assertEqual(tile.value,10)
+            self.assertEqual(tile.cant,1)
 
 class TestBagTiles(unittest.TestCase):
     @patch ('random.shuffle')
