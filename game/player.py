e4626e0f16e06e277d8cf48b9af515dfeb1be0a9
vicky <vicky@pop-os.localdomain>
2023-09-01 15:37:49 -0300

    agrego player

--- game/player.py
+++ game/player.py
@@ -1,3 +1,7 @@
 class Player:
-    def __init__(self):
-        self.tiles = []
+    def __init__(self, bag_tiles):
+        self.tiles = bag_tiles.take(7)
+        self.bag_tiles = bag_tiles
+    
+    def rellenar(self):
+        self.tiles += bag_tiles.take
