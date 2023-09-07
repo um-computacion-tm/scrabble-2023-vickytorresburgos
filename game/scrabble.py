d6faed6109aecfb5f91e37112dddcef9e31b7830
vicky <vicky@pop-os.localdomain>
2023-09-07 09:16:23 -0300

    game/scrabble

--- game/scrabble.py
+++ game/scrabble.py
@@ -1,9 +1,20 @@
 from game.board import Board
-from game.board import Player
+from game.player import Player
 from game.models import BagTiles
 
 class ScrabbleGame:
-    def __init__(self, players_count):
+    def __init__(self, players_count: int):
         self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players:list[Player] = []
         for _ in range(players_count):
-            self.players.append(Player())
+            self.players.append(Player(bag_tiles=self.bag_tiles))
+        self.current_player = None
+
+    def next_turn(self):
+        if self.current_player is None:
+            self.current_player = self.players[0]
+        else:
+            index = self.players.index(self.current_player) + 1
+            self.current_player = self.players[index]
+
