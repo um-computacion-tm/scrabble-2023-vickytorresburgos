0ac1c2065e7c88409367298d5b7594afd801b565
vicky <vicky@pop-os.localdomain>
2023-09-09 13:18:26 -0300

    test board

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,4 @@
 import unittest
-
 from game.board import Board
 
 class TestBoard(unittest.TestCase):
@@ -9,12 +8,23 @@ class TestBoard(unittest.TestCase):
             len(board.grid),
             15,
         )
-
         self.assertEqual(
             len(board.grid[0]),
             15,
         )
 
-if __name__ == '__main__':
-    unittest.main()
+def test_word_inside_board(self):
+        board = Board()
+        word = "Facultad"
+        location = (5, 4)
+        orientation = "H"
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == True
 
+def test_word_out_of_board(self):
+        board = Board()
+        word = "Facultad"
+        location = (14, 4)
+        orientation = "H"
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == False
