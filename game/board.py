40aa8f3015197494a1dd40ea9bfd42662b979da0
vicky <vicky@pop-os.localdomain>
2023-09-27 10:37:17 -0300

    add board

--- game/board.py
+++ game/board.py
@@ -1,28 +1,34 @@
+from game.cell import Cell
 class Board:
- # def __init__(self):
- #       self.grid = [[Cell(1, "", "") for _ in range(15)] for _ in range(15)]
+    def __init__(self):
+        self.grid = [
+            [Cell(1, "") for _ in range(15)] 
+            for _ in range(15)
+            ]
 
-    # @staticmethod
-    # def calculate_word_value(word: list[Cell]) -> int:
-    #     value: int = 0
-    #     multiplier_word = None
-    #     for cell in word:
-    #         value = value + cell.calculate_value()
-    #         if cell.multiplier_type == "word" and cell.active:
-    #             multiplier_word = cell.multiplier
-    #     if multiplier_word:
-    #         value = value * multiplier_word
-    #     return value
+    def calculate_word_value(word: list[Cell]) -> int:
+        value: int = 0
+        multiplier_word = None
+        for cell in word:
+            value = value + cell.calculate_value()
+            if cell.multiplier_type == "word" and cell.active:
+                multiplier_word = cell.multiplier
+        if multiplier_word:
+            value = value * multiplier_word
+        return value
     
     def validate_word_inside_board(self, word, location, orientation):
-        #WORK IN PROGRESS
-        position_x = location[0]
-        position_y = location[1]
         len_word = len(word)
-        if orientation == "H":
-            if position_x + len_word > 15:
+        posicion_x = location[0]
+        posicion_y = location[1]
+
+        if orientation == 'H':
+            if posicion_x + len_word > 15:
                 return False
             else:
                 return True
-        else:
-            pass
+        elif orientation == 'V':
+            if posicion_y + len_word > 15:
+                return False
+            else:
+                return True
