e07cb6860b23100b4d24dc0391c1c7b18d959d5c
vicky <vicky@pop-os.localdomain>
2023-10-01 18:41:17 -0300

    updated cell

--- game/cell.py
+++ game/cell.py
@@ -1,10 +1,12 @@
+from game.models import Tile
+
 class Cell:
     def __init__(self, multiplier, multiplier_type):
         self.multiplier = multiplier
         self.multiplier_type = multiplier_type
         self.letter = None
 
-    def add_letter(self, letter):
+    def add_letter(self, letter:Tile):
         self.letter = letter
 
     def calculate_value(self):
@@ -14,3 +16,14 @@ class Cell:
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
+        
+    def calculate_word_value(self,word):
+        word_value = 0
+        word_multiplier = 1
+        for letter in word:
+            word_value += letter.calculate_value()
+            if letter.multiplier_type == 'word':
+                word_multiplier = letter.multiplier
+                letter.multiplier_type = None
+        word_value *= word_multiplier
+        return word_value
