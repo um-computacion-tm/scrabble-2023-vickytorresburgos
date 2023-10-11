ff0579ea3ad3f3ff2706d0ac33d1daabaf791463
vicky <vicky@pop-os.localdomain>
2023-10-11 08:58:48 -0300

    update tests

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -13,78 +13,78 @@ class TestCell(unittest.TestCase):
     def test_add_letter(self):
         cell = Cell(multiplier=1, multiplier_type='',letter=None)
         letter = Tile(letter='p', value=3, cant=2)
-        cell.add_letter(tile=letter)
+        cell.add_letter(letter)
         self.assertEqual(cell.letter, letter)
 
     def test_cell_value(self):
         cell = Cell(multiplier=2, multiplier_type='letter')
         letter = Tile(letter='p', value=3, cant=2)
-        cell.add_letter(tile=letter)
+        cell.add_letter(letter)
         self.assertEqual(cell.calculate_value(),6)
 
     def test_cell_multiplier_word(self):
         cell = Cell(multiplier=2, multiplier_type='word')
         letter = Tile(letter='p', value=3, cant=2)
-        cell.add_letter(tile=letter)
+        cell.add_letter(letter)
         self.assertEqual(cell.calculate_value(),3)
 
-# class TestCalculateValue(unittest.TestCase):
-#     def test_simple(self):
-#         cell = Cell(multiplier=None, multiplier_type='')
-#         word = [
-#             Cell(letter=Tile('C',1,4),multiplier_type='', multiplier=None),
-#             Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
-#             Cell(letter=Tile('S',2,6),multiplier_type='', multiplier=None),
-#             Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None)
-#         ]
-#         value = cell.calculate_word_value(word)
-#         self.assertEqual(value, 5)
+class TestCalculateValue(unittest.TestCase):
+    def test_simple(self):
+        cell = Cell(multiplier=None, multiplier_type='')
+        word = [
+            Cell(letter=Tile('C',1,4),multiplier_type='', multiplier=None),
+            Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
+            Cell(letter=Tile('S',2,6),multiplier_type='', multiplier=None),
+            Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None)
+        ]
+        value = cell.calculate_word_value(word)
+        self.assertEqual(value, 5)
 
-#     def test_with_letter_multiplier(self):
-#         cell = Cell(multiplier=None, multiplier_type='')
-#         word = [
-#             Cell(letter=Tile('C',1,4),multiplier_type='', multiplier=None),
-#             Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
-#             Cell(letter=Tile('S',2,6),multiplier = 2, multiplier_type ='letter'),
-#             Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
-#         ]
-#         value = cell.calculate_word_value(word)
-#         self.assertEqual(value, 7)
+    def test_with_letter_multiplier(self):
+        cell = Cell(multiplier=None, multiplier_type='')
+        word = [
+            Cell(letter=Tile('C',1,4),multiplier_type='', multiplier=None),
+            Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
+            Cell(letter=Tile('S',2,6),multiplier = 2, multiplier_type ='letter'),
+            Cell(letter=Tile('A',1,12),multiplier_type='', multiplier=None),
+        ]
+        value = cell.calculate_word_value(word)
+        self.assertEqual(value, 7)
 
-#     def test_with_word_multiplier(self):
-#         cell = Cell(multiplier=None, multiplier_type='')
-#         word = [
-#             Cell(letter=Tile('C',1,4),multiplier = None, multiplier_type =''),
-#             Cell(letter=Tile('A',1,12),multiplier = None, multiplier_type =''),
-#             Cell(letter=Tile('S',2,6), multiplier = 2, multiplier_type ='word'),
-#             Cell(letter=Tile('A',1,12),multiplier = None, multiplier_type =''),
-#         ]
-#         value = cell.calculate_word_value(word)
-#         self.assertEqual(value, 10)
+    def test_with_word_multiplier(self):
+        cell = Cell(multiplier=None, multiplier_type='')
+        word = [
+            Cell(letter=Tile('C',1,4),multiplier = None, multiplier_type =''),
+            Cell(letter=Tile('A',1,12),multiplier = None, multiplier_type =''),
+            Cell(letter=Tile('S',2,6), multiplier = 2, multiplier_type ='word'),
+            Cell(letter=Tile('A',1,12),multiplier = None, multiplier_type =''),
+        ]
+        value = cell.calculate_word_value(word)
+        self.assertEqual(value, 10)
 
-#     def test_with_letter_word_multiplier(self):
-#         cell = Cell(multiplier=None, multiplier_type='')
-#         word = [
-#             Cell(letter=Tile('C', 1,4),multiplier=3,multiplier_type='letter'),
-#             Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
-#             Cell(letter=Tile('S', 2,6), multiplier=2,multiplier_type='word'),
-#             Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
-#         ]
-#         value = cell.calculate_word_value(word)
-#         self.assertEqual(value, 14)
+    def test_with_letter_word_multiplier(self):
+        cell = Cell(multiplier=None, multiplier_type='')
+        word = [
+            Cell(letter=Tile('C', 1,4),multiplier=3,multiplier_type='letter'),
+            Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
+            Cell(letter=Tile('S', 2,6), multiplier=2,multiplier_type='word'),
+            Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
+        ]
+        value = cell.calculate_word_value(word)
+        self.assertEqual(value, 14)
 
-#     def test_with_letter_word_multiplier_active_and_inactive(self):
-#         cell = Cell(multiplier=None, multiplier_type='')
-#         word = [
-#             Cell(letter=Tile('C', 1,4),multiplier=3, multiplier_type='letter'),
-#             Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
-#             Cell(letter=Tile('S', 2,6), multiplier=2,multiplier_type='word'),
-#             Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
-#         ]
-#         value1 = cell.calculate_word_value(word) #active cells
-#         value2 = cell.calculate_word_value(word) #inactive cells
-#         self.assertEqual(value1, 14)
-#         self.assertEqual(value2, 5)
+    def test_with_letter_word_multiplier_active_and_inactive(self):
+        cell = Cell(multiplier=None, multiplier_type='')
+        word = [
+            Cell(letter=Tile('C', 1,4),multiplier=3, multiplier_type='letter'),
+            Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
+            Cell(letter=Tile('S', 2,6), multiplier=2,multiplier_type='word'),
+            Cell(letter=Tile('A', 1,12),multiplier = None, multiplier_type =''),
+        ]
+        value1 = cell.calculate_word_value(word) #active cells
+        value2 = cell.calculate_word_value(word) #inactive cells
+        self.assertEqual(value1, 14)
+        self.assertEqual(value2, 5)
 
 if __name__ == '__main__':
     unittest.main()
