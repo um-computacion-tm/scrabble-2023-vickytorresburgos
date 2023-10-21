dab31cc6d792e92bc63e93cff52126ccc6d4a5ac
vicky <vicky@pop-os.localdomain>
2023-10-21 16:33:44 -0300

    add main

--- game/main.py
+++ game/main.py
@@ -1,27 +1,27 @@
-# from game.scrabble import ScrabbleGame
+from game.scrabble import ScrabbleGame
 
-# def main():
-#     print("Bienvenido!")
-#     while True:
-#         try: 
-#             players_count = int(input("Ingrese cantidad de jugadores: "))
-#             if players_count <= 1 or players_count > 4:
-#                 raise ValueError
-#             break
-#         except ValueError:
-#             print("Valor invalido")
-#     scrabble_game = ScrabbleGame(players_count=players_count)
-#     print("Cantidad de jugadores: ",len(scrabble_game.players))
-#     scrabble_game.next_turn()
-#     #TODO while playing: loop por turno de jugador hasta que termine el juego
-#     print(f"Turno del jugador {scrabble_game.current_player.id}")
-#     word = input("Ingrese palabra: ")
-#     location_x = input("Ingrese posicion X: ")
-#     location_y = input("Ingrese posicion Y: ")
-#     location = (location_x, location_y)
-#     orientation = input("Ingrese orientacion (V/H)")
-#     scrabble_game.validate_word(word, location, orientation)
+def main():
+    print("Bienvenido!")
+    while True:
+        try: 
+            players_count = int(input("Ingrese cantidad de jugadores: "))
+            if players_count <= 1 or players_count > 4:
+                raise ValueError
+            break
+        except ValueError:
+            print("Valor invalido")
+    scrabble_game = ScrabbleGame(players_count=players_count)
+    print("Cantidad de jugadores: ",len(scrabble_game.players))
+    scrabble_game.next_turn()
+    #TODO while playing: loop por turno de jugador hasta que termine el juego
+    print(f"Turno del jugador {scrabble_game.current_player.id}")
+    word = input("Ingrese palabra: ")
+    location_x = input("Ingrese posicion X: ")
+    location_y = input("Ingrese posicion Y: ")
+    location = (location_x, location_y)
+    orientation = input("Ingrese orientacion (V/H)")
+    scrabble_game.validate_word(word, location, orientation)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
