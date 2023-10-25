62b2b898ac362edc7df2d6f47a9afd74579d94d6
vicky <vicky@pop-os.localdomain>
2023-10-25 15:00:54 -0300

    update player

--- game/player.py
+++ game/player.py
@@ -1,5 +1,6 @@
 from game.models import BagTiles,Tile
 from collections import Counter
+from game.board import Board 
 
 class NotEnoughTilesException(Exception):
     pass
@@ -10,27 +11,34 @@ class Player:
         self.score = 0
         self.tiles = []
 
-    def take_tiles(self, bag: BagTiles, amount):
+    def get_tiles(self, bag: BagTiles, amount):
         self.tiles.extend(bag.take(amount))
 
     def show_tiles(self):
         return self.tiles
     
     def has_letters(self, tiles=[]):
-        letras_jugador = [tile.letter for tile in self.tiles]
+        player_tiles = [tile.letter for tile in self.tiles]
         # Transform tiles into letters
-        letras_palabra = [tile.letter for tile in tiles]
+        word_tiles = [tile.letter for tile in tiles]
         # convert into counter to count needed letters
-        letras_necesarias = Counter(letras_palabra)
+        needed_tiles = Counter(word_tiles)
         # Compare with the letters available to the player
-        for letra, cantidad in letras_necesarias.items():
-            if letras_jugador.count(letra) < cantidad:
+        for letter, quantity in needed_tiles.items():
+            if player_tiles.count(letter) < quantity:
                 return False
         return True
     
+    def remove_letters_from_hand(self, word):
+        for letter in word:
+            for tile in self.tiles:
+                if tile == letter:
+                    self.tiles.remove(tile)
+                    break
+    
     def exchange_tiles(self, bag, tiles_to_exchange):
         if len(self.tiles) < tiles_to_exchange: #tiles the player want to exchange
-            raise NotEnoughTilesException(f"No hay suficientes fichas para intercambiar ({len(self.tiles)} < {tiles_to_exchange})")
+            raise NotEnoughTilesException(f"There is not enough tiles to exchange: ({len(self.tiles)} < {tiles_to_exchange})")
         # takes player tiles
         exchanged_tiles = self.tiles[:tiles_to_exchange]
         self.tiles = self.tiles[tiles_to_exchange:]
@@ -50,4 +58,3 @@ class Player:
 
     def get_score(self):
         return self.score
-    
